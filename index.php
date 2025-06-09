48861eff87910c86e8f85786fd2157fd7f40722f
helen2411 <helenlaisgomes24@gmail.com>
2025-06-09 17:34:03 -0300

    Divino Donuts

--- index.php
+++ index.php
@@ -6,10 +6,62 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Divino Donuts</title>
     <link rel="sylesheet" href="styles/all.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&family=Pacifico&display=swap" rel="stylesheet">
 </head>
 <body>
 
+    <p>Hello</p>
+
+    <header>
+        <div class="accessibility">
+            <a href="#" aria-label="Acessibilidade">
+                
+            </a>
+        </div>
     
-    
+    <div class="logo">
+    <img src="">
+    </div>
+
+    <div class="login">
+            <a href="#">Login</a>
+        </div>
+    </header>
+
+    <main>
+        <section class="top-section">
+            <div class="content-box white-border">
+                <div class="dots-container">
+                    <span class="dot"></span>
+                    <span class="dot"></span>
+                    <span class="dot"></span>
+                </div>
+            </div>
+        </section>
+
+    <section class="about-us-section">
+            <h2>Sobre Nós</h2>
+            <div class="content-box large-box">
+                <p>
+                    Bem-vindo à Divino Donuts! Somos apaixonados por criar os donuts mais deliciosos e criativos que você já provou. Cada um dos nossos donuts é feito com ingredientes frescos e amor, resultando em uma explosão de sabor a cada mordida.
+                    Nossa missão é trazer alegria aos seus dias com doces irresistíveis e momentos inesquecíveis. Seja para um café da manhã especial, um lanche da tarde ou para compartilhar com amigos e família, temos o donut perfeito para você.
+                    Explore nossa variedade de sabores, desde os clássicos açucarados até as criações mais inovadoras e coloridas. Na Divino Donuts, a felicidade é doce e redonda!
+                </p>
+            </div>
+            <div class="three-boxes-container">
+                <div class="small-box yellow-border"></div>
+                <div class="small-box blue-border"></div>
+                <div class="small-box pink-border"></div>
+            </div>
+        </section>
+    </main>
+
+    <footer>
+        </footer>
+
 </body>
 </html>
+
+
